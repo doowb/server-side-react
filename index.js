d129fb7ba78b7585ea356b74c4e7fa5337802e14
Brian Woodward <brian.woodward@gmail.com>
2015-03-02 23:21:13 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * server-side-react <https://github.com/doowb/server-side-react>
+ *
+ * Copyright (c) 2015 Brian Woodward.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
